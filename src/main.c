d237e8ddbdbe06ed713fef6a6c8b9ed0621643ea
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-04-30 18:19:10 -0500

    Add color buffer

--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_pixels.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
 #include <stdio.h>
 
+#define DEFAULT_MONITOR_NUMBER -1
+
 bool is_running = false;
+
 uint16_t WINDOW_WIDTH = 2560;
 uint16_t WINDOW_HEIGHT = 1440;
+
 uint32_t *color_buffer = NULL;
+SDL_Texture *color_buffer_texture = NULL;
+
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
@@ -25,7 +32,7 @@ bool init_window(void) {
     return false;
   }
 
-  renderer = SDL_CreateRenderer(window, -1, 0); // -1 default monitor num
+  renderer = SDL_CreateRenderer(window, DEFAULT_MONITOR_NUMBER, 0);
   if (!renderer) {
     fprintf(stderr, "SDL renderer failed to initialize!");
     return false;
@@ -37,6 +44,10 @@ bool init_window(void) {
 void setup(void) {
   color_buffer =
       (uint32_t *)malloc(sizeof(uint32_t) * WINDOW_WIDTH * WINDOW_HEIGHT);
+
+  color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+                                           SDL_TEXTUREACCESS_STREAMING,
+                                           WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 void process_input(void) {
@@ -60,9 +71,27 @@ void update(void) {
   // TODO:
 }
 
+void render_color_buffer(void) {
+  SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer,
+                    (uint32_t)(WINDOW_WIDTH * sizeof(uint32_t)));
+
+  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+  for (size_t row = 0; row < WINDOW_HEIGHT; row++) {
+    for (size_t col = 0; col < WINDOW_WIDTH; col++) {
+      color_buffer[(WINDOW_WIDTH * row) + col] = color;
+    }
+  }
+}
+
 void render(void) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
   SDL_RenderClear(renderer);
+
+  render_color_buffer();
+  clear_color_buffer(0xFFFFFF00);
 
   // double buffer
   SDL_RenderPresent(renderer);
