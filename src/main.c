56f58e910ca20cb17e25009838eccc1378edd151
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-05-01 15:55:45 -0500

    Add cube_points, projected points, and related items for drawing 3D-ish cubes

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include "./display.h"
+#include "./vector.h"
+
+#define NUM_OF_POINTS (9 * 9 * 9)
 
 bool is_running = false;
+float fov_factor = 128;
 
+vec3_t cube_points[NUM_OF_POINTS];
+vec2_t projected_points[NUM_OF_POINTS];
 void setup(void) {
   color_buffer =
       (uint32_t *)malloc(sizeof(uint32_t) * WINDOW_WIDTH * WINDOW_HEIGHT);
@@ -9,10 +15,32 @@ void setup(void) {
   color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
                                            SDL_TEXTUREACCESS_STREAMING,
                                            WINDOW_WIDTH, WINDOW_HEIGHT);
+  uint32_t point_count = 0;
+  for (float x = -1; x <= 1; x += 0.25) {
+    for (float y = -1; y <= 1; y += 0.25) {
+      for (float z = -1; z <= 1; z += 0.25) {
+        vec3_t new_point = {x, y, z};
+        cube_points[point_count++] = new_point;
+      }
+    }
+  }
+}
+
+vec2_t project(vec3_t point) {
+  vec2_t projected_point = {
+      (fov_factor * point.x),
+      (fov_factor * point.y),
+  };
+  return projected_point;
 }
 
 void update(void) {
-  // TODO:
+  for (size_t i = 0; i < NUM_OF_POINTS; i++) {
+    vec3_t point = cube_points[i];
+    vec2_t projected_point = project(point);
+
+    projected_points[i] = projected_point;
+  }
 }
 
 void process_input(void) {
@@ -33,13 +61,17 @@ void process_input(void) {
 }
 
 void render(void) {
-  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-  SDL_RenderClear(renderer);
+  draw_grid();
+
+  for (size_t i = 0; i < NUM_OF_POINTS; i++) {
+    vec2_t projected_point = projected_points[i];
+    draw_rectangle(projected_point.x + (uint16_t)(WINDOW_WIDTH / 2),
+                   projected_point.y + (uint16_t)(WINDOW_HEIGHT / 2), 4, 4,
+                   0xFFFF0000);
+  }
 
   render_color_buffer();
   clear_color_buffer(0xFF000000);
-  draw_grid();
-  draw_rectangle(800, 800, 300, 50, 0xFFFFFF00);
 
   // double buffer
   SDL_RenderPresent(renderer);
@@ -49,6 +81,8 @@ int main(void) {
   is_running = init_window();
 
   setup();
+
+  vec3_t vector = {2.0, 3.0, -4.0};
 
   while (is_running) {
     process_input();
