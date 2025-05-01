2fab2294f76f2cbcb09b443563f1b3f7323acef8
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-04-30 22:16:36 -0500

    Remove display functions from main

--- src/main.c
+++ src/main.c
@@ -1,57 +1,6 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_pixels.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_video.h>
-#include <stdbool.h>
-#include <stdio.h>
-
-#define DEFAULT_MONITOR_NUMBER -1
+#include "./display.h"
 
 bool is_running = false;
-
-uint16_t WINDOW_WIDTH = 0;
-uint16_t WINDOW_HEIGHT = 0;
-
-uint32_t *color_buffer = NULL;
-SDL_Texture *color_buffer_texture = NULL;
-
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-
-bool init_window(void) {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    fprintf(stderr, "SDL failed to initialize!\n");
-    return false;
-  }
-
-  SDL_DisplayMode display_mode;
-  SDL_GetCurrentDisplayMode(0, &display_mode); // 0 is main monitor
-
-  WINDOW_WIDTH = display_mode.w;
-  WINDOW_HEIGHT = display_mode.h;
-
-  window =
-      SDL_CreateWindow("3D Renderer", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-  if (!window) {
-    fprintf(stderr, "SDL window failed to initialize!\n");
-    return false;
-  }
-
-  renderer = SDL_CreateRenderer(window, DEFAULT_MONITOR_NUMBER, 0);
-  if (!renderer) {
-    fprintf(stderr, "SDL renderer failed to initialize!\n");
-    return false;
-  }
-
-  if (SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN) != 0) {
-    fprintf(stderr, "SDL failed to make the window fullscreen!\n");
-    return false;
-  }
-  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
-  return true;
-}
 
 void setup(void) {
   color_buffer =
@@ -60,6 +9,10 @@ void setup(void) {
   color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
                                            SDL_TEXTUREACCESS_STREAMING,
                                            WINDOW_WIDTH, WINDOW_HEIGHT);
+}
+
+void update(void) {
+  // TODO:
 }
 
 void process_input(void) {
@@ -79,41 +32,6 @@ void process_input(void) {
   }
 }
 
-void update(void) {
-  // TODO:
-}
-
-void render_color_buffer(void) {
-  SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer,
-                    (uint32_t)(WINDOW_WIDTH * sizeof(uint32_t)));
-
-  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
-
-void draw_grid(void) {
-  for (size_t row = 0; row < WINDOW_HEIGHT; row += 10) {
-    for (size_t col = 0; col < WINDOW_WIDTH; col += 10)
-      color_buffer[(WINDOW_WIDTH * row) + col] = 0xFF555555;
-  }
-}
-
-void draw_rectangle(uint32_t x, uint32_t y, uint16_t width, uint16_t height,
-                    uint32_t color) {
-  for (size_t row = 0; row < height; row++) {
-    for (size_t col = 0; col < width; col++) {
-      color_buffer[(WINDOW_WIDTH * row + x) + col + y * y] = color;
-    }
-  }
-}
-
-void clear_color_buffer(uint32_t color) {
-  for (size_t row = 0; row < WINDOW_HEIGHT; row++) {
-    for (size_t col = 0; col < WINDOW_WIDTH; col++) {
-      color_buffer[(WINDOW_WIDTH * row) + col] = color;
-    }
-  }
-}
-
 void render(void) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
   SDL_RenderClear(renderer);
@@ -125,13 +43,6 @@ void render(void) {
 
   // double buffer
   SDL_RenderPresent(renderer);
-}
-
-void destroy_window(void) {
-  free(color_buffer);
-  SDL_DestroyWindow(window);
-  SDL_DestroyRenderer(renderer);
-  SDL_Quit();
 }
 
 int main(void) {
