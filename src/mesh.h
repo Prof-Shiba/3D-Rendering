16253b716005cd8c7faaa000ac6c8e3b075023f4
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-05-10 17:33:22 -0500

    Add mesh_t type that takes vertices and faces

--- src/mesh.h
+++ src/mesh.h
@@ -3,7 +3,14 @@
 #include "./vector.h"
 
 #define NUM_CUBE_VERTICES 8
-extern vec3_t cube_vertices[NUM_CUBE_VERTICES];
-
 #define NUM_CUBE_FACES (6 * 2) // 6 faces, 2 triangles per face
+
+extern vec3_t cube_vertices[NUM_CUBE_VERTICES];
 extern face_t cube_faces[NUM_CUBE_FACES];
+
+typedef struct {
+  vec3_t *vertices;
+  face_t *faces;
+} mesh_t;
+
+extern mesh_t mesh;
