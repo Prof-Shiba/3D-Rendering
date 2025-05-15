ed3baeccd664ed28615047aae03dc7664bacf8f7
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-05-14 22:08:46 -0500

    Remove hardcoded cube values. Add load_obj_data implementation

--- src/mesh.c
+++ src/mesh.c
@@ -1,46 +1,38 @@
 #include "./mesh.h"
 #include "array.h"
-#include <wchar.h>
+#include <stdio.h>
+#include <string.h>
 
 mesh_t mesh = {.vertices = NULL, .faces = NULL, .rotation = {0, 0, 0}};
 
-vec3_t cube_vertices[NUM_CUBE_VERTICES] = {
-    {-1, -1, -1}, {-1, 1, -1}, {1, 1, -1}, {1, -1, -1},
-    {1, 1, 1},    {1, -1, 1},  {-1, 1, 1}, {-1, -1, 1}};
-
-face_t cube_faces[NUM_CUBE_FACES] = {
-    // front
-    {1, 2, 3},
-    {1, 3, 4},
-    // right
-    {4, 3, 5},
-    {4, 5, 6},
-    // back
-    {6, 5, 7},
-    {6, 7, 8},
-    // left
-    {8, 7, 2},
-    {8, 2, 1},
-    // top
-    {2, 7, 5},
-    {2, 5, 3},
-    // bottom
-    {6, 8, 1},
-    {6, 1, 4}};
-
-void load_cube_mesh_data(void) {
-  for (uint32_t i = 0; i < NUM_CUBE_VERTICES; i++) {
-    vec3_t cube_vertex = cube_vertices[i];
-    array_push(mesh.vertices, cube_vertex);
+void load_obj_data(char *file_src) {
+  FILE *file = fopen(file_src, "r");
+  if (!file) {
+    fprintf(stderr, "Failed to open file %s! (check the path?)\n", file_src);
+    return;
   }
 
-  for (uint32_t i = 0; i < NUM_CUBE_FACES; i++) {
-    face_t cube_face = cube_faces[i];
-    array_push(mesh.faces, cube_face);
-  }
-}
+  char buffer[128];
+  while (fgets(buffer, sizeof(buffer), file)) {
 
-void load_obj_data(char *file_name) {
-  // TODO: Load vertices and faces in
-  // mesh.vertices and mesh.faces
+    if (strncmp(buffer, "v ", 2) == 0) {
+      vec3_t vertex;
+      sscanf(buffer, "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
+      array_push(mesh.vertices, vertex);
+
+    } else if (strncmp(buffer, "f ", 2) == 0) {
+      int32_t vertex_indices[3];
+      int32_t texture_indices[3];
+      int32_t normal_indices[3];
+
+      sscanf(buffer, "f %d/%d/%d %d/%d/%d %d/%d/%d", &vertex_indices[0],
+             &texture_indices[0], &normal_indices[0], &vertex_indices[1],
+             &texture_indices[1], &normal_indices[1], &vertex_indices[2],
+             &texture_indices[2], &normal_indices[2]);
+
+      face_t face = {vertex_indices[0], vertex_indices[1], vertex_indices[2]};
+      array_push(mesh.faces, face);
+    }
+  }
+  fclose(file);
 }
--- src/mesh.h
+++ src/mesh.h
@@ -2,12 +2,6 @@
 #include "./triangle.h"
 #include "./vector.h"
 
-#define NUM_CUBE_VERTICES 8
-#define NUM_CUBE_FACES (6 * 2) // 6 faces, 2 triangles per face
-
-extern vec3_t cube_vertices[NUM_CUBE_VERTICES];
-extern face_t cube_faces[NUM_CUBE_FACES];
-
 typedef struct {
   vec3_t *vertices;
   face_t *faces;
@@ -16,4 +10,4 @@ typedef struct {
 
 extern void load_cube_mesh_data(void);
 extern mesh_t mesh;
-extern void load_obj_data(char *file_name);
+extern void load_obj_data(char *file_src);
