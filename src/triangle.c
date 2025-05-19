745b70fe7570df83cef23fd1a0cec3980c07f461
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-05-19 12:22:04 -0500

    add: fill_top_tri implementation and fix triangles to use passed in color

--- src/triangle.c
+++ src/triangle.c
@@ -23,8 +23,21 @@ void fill_bottom_tri(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
     x_end += inv_slope_2;
   }
 }
+
 void fill_top_tri(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
-                  uint16_t x2, uint16_t y2, uint32_t color) {}
+                  uint16_t x2, uint16_t y2, uint32_t color) {
+  float inv_slope_1 = (float)(x2 - x0) / (y2 - y0);
+  float inv_slope_2 = (float)(x2 - x1) / (y2 - y1);
+
+  float x_start = x2;
+  float x_end = x2;
+
+  for (size_t y = y2; y >= y0; y--) {
+    draw_line(x_start, y, x_end, y, color);
+    x_start -= inv_slope_1;
+    x_end -= inv_slope_2;
+  }
+}
 
 // draws a filled triangle using flat-top/flat-bottom method
 // splits tri in 2, (bottom and top), then fills each portion
@@ -46,6 +59,6 @@ void draw_filled_triangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
   uint16_t My = y1;
   uint16_t Mx = ((x2 - x0) * (y1 - y0)) / (y2 - y0) + x0;
 
-  fill_bottom_tri(x0, y0, x1, y1, Mx, My, 0xFF00FF00);
-  fill_top_tri(x1, y1, Mx, My, x2, y2, 0xFF00FF00);
+  fill_bottom_tri(x0, y0, x1, y1, Mx, My, color);
+  fill_top_tri(x1, y1, Mx, My, x2, y2, color);
 }
