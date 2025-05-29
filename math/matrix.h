6ee9f1890b5f683a1f0dc82d2ef7ea1ca3bad618
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-05-29 12:27:22 -0500

    Add mat4_scale and mat4_identity declaration and implementation

--- math/matrix.h
+++ math/matrix.h
@@ -1,2 +1,11 @@
 #pragma once
 
+// scale, translation, rotation
+// translation requires 4x4, not 3x3
+// x, y, z, w; w->1 (initially)
+typedef struct {
+  float m[4][4];
+} mat4_t;
+
+mat4_t mat4_identity(void);
+mat4_t mat4_scale(float scale_x, float scale_y, float scale_z);
