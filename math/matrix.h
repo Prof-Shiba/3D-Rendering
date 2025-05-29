a575807b34f0c0ccc26a136b1d5d827d8bdd2f5e
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-05-29 13:03:58 -0500

    add: mat4_mul_vec4 declaration

--- math/matrix.h
+++ math/matrix.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../math/vector.h"
 
 // scale, translation, rotation
 // translation requires 4x4, not 3x3
@@ -9,3 +10,4 @@ typedef struct {
 
 mat4_t mat4_identity(void);
 mat4_t mat4_scale(float scale_x, float scale_y, float scale_z);
+vec4_t mat4_mul_vec4(mat4_t m, vec4_t v);
