e494f6bb2d1ba92b9c96031791d059826ea47d8d
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-06-05 17:44:48 -0500

    increase red vertex dot size

--- src/main.c
+++ src/main.c
@@ -256,11 +256,11 @@ void render(void) {
     // red vertex dots
     if (render_modes == 2) {
       draw_rectangle(current_triangle.points[0].x, current_triangle.points[0].y,
-                     3, 3, 0xFFFF0000);
+                     4, 4, 0xFFFF0000);
       draw_rectangle(current_triangle.points[1].x, current_triangle.points[1].y,
-                     3, 3, 0xFFFF0000);
+                     4, 4, 0xFFFF0000);
       draw_rectangle(current_triangle.points[2].x, current_triangle.points[2].y,
-                     3, 3, 0xFFFF0000);
+                     4, 4, 0xFFFF0000);
     }
   }
 
